e95fa1b3a98a3a85f21dbc27b52288843a284b4c
Christina Dob <cd@cd.local>
2019-04-17 12:41:57 -0700

    Week 7 Day 3 Mini

--- src/components/HackerNews/HackerNews.js
+++ src/components/HackerNews/HackerNews.js
@@ -1,30 +1,50 @@
 import React, { Component } from 'react';
 import Card from './../shared/Card/Card';
 import Loading from './../shared/Loading/Loading';
+// connect is a higher order function that will accept a coponent and return
+import { connect } from 'react-redux';
+import { requestArticles } from './../../ducks/hackerNewsReducer';
 
 class HackerNews extends Component {
   constructor(props) {
     super(props);
-    this.state = { articles: [], loading: true }
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.props.requestArticles();
   }
 
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    console.log(this.props);
+    const articles = this.props.articles.map(article => (
+      <Card key={article.id} article={article} />
+    ));
     return (
-      <div className='news-container'>
+      <div className="news-container">
         <img style={styles.logo} src="./hackerNews.jpeg" alt="" />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+        {this.props.loading ? <Loading /> : <div>{articles}</div>}
       </div>
-    )
+    );
   }
 }
 
-export default HackerNews;
+function mapStateToProps(state) {
+  return state.hackerNews;
+}
 
+const mapDispatchToProps = {
+  requestArticles
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HackerNews);
 
 const styles = {
   logo: {
     width: '250px',
     margin: '50px 0px'
   }
-}
+};
